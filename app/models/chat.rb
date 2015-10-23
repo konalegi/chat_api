e5987a969b22423949239f16ca5fe5da1e1ef235
Ramil Sitdikov <sitdikov.ramil@gmail.com>
2015-10-23 13:26:39 +0300

    Change association for users and chats from has_many to has_many_through

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,4 +1,4 @@
 class Chat < ActiveRecord::Base
   has_many :messages
-  has_many :users
+  has_many :users, through: :messages
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :messages
-  has_many :chats
+  has_many :chats, through: :messages
 
   validates :name, presence: true
   validates :email, presence: true
