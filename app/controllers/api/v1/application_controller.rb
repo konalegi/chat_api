94e0912b4ca8b96486f6183a33d9f711700e1b16
Ramil Sitdikov <sitdikov.ramil@gmail.com>
2015-10-26 15:08:53 +0300

    Move app_controller to api folder. Add current_user

---
+++ app/controllers/api/v1/application_controller.rb
@@ -0,0 +1,21 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :authenticate
+  helper_method :current_user
+
+  TOKEN = {
+    "first": 1,
+    "second": 2,
+    "third": 3
+  }
+
+  def current_user
+    @current_user ||= User.find session[:user_id] if session[:user_id]
+  end
+
+  def authenticate
+    authenticate_or_request_with_http_token do |token|
+      TOKEN.has_key?(token)
+    end
+  end
+end
