91de2b978806225311a633dc3f3fb00350dfd7a0
av <av@fun-box.ru>
2015-10-29 17:49:28 +0300

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,7 @@ Rails.application.routes.draw do
   # Api definition
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-      resources :users
-      resources :messages
-      resources :chats
+      resources :chats, only: [:index, :create, :show]
     end
   end
 end
