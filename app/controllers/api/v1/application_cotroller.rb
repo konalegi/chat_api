97c50e9bb56556e2a390b975478e91d167a3921d
av <av@fun-box.ru>
2015-10-29 15:02:04 +0300

    Move application controller to api/v1 folder

--- app/controllers/api/v1/application_cotroller.rb
+++ app/controllers/api/v1/application_cotroller.rb
@@ -1,6 +1,6 @@
-class ApplicationController < ActionController::Base
+class API::V1::ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  #before_action :authenticate
+  before_action :authenticate
   helper_method :current_user
 
   TOKEN = {
@@ -8,9 +8,12 @@ class ApplicationController < ActionController::Base
     "second": 2,
     "third": 3
   }
-
+protected
   def current_user
     @current_user ||= User.find session[:user_id] if session[:user_id]
+  end
+
+  def current_user!#should throw exception forbidden
   end
 
   def authenticate
---
+++ test/controllers/application_controller_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class ApplicationControllerTest < ActionController::TestCase
+  test "current_user" do
+    user = FactoryGirl.create9(:user)
+
+  end
+  test "authentication" do
+  end
+end
