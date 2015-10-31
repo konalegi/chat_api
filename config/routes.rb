90b874ea87bab5a0c1c5cba2abac118b7c34262f
av <av@fun-box.ru>
2015-10-31 15:59:07 +0300

    Add views to chats, change routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 require 'api_constraints'
 
 Rails.application.routes.draw do
-
-  # Api definition
+  # Api definition"
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-      resources :chats, only: [:index, :create, :show]
+      scope module: :chats do
+        resources :chats, only: [:index, :create, :show]
+      end
     end
   end
 end
